12dcf535cc069450e163697a9449d376e85e6d27
Pedro Prado Win <126698557+pradoPedroPrado@users.noreply.github.com>
2023-03-15 20:53:12 -0300

    Homework 7, no padding yet

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+using System.ComponentModel;
+using System.Text;
+
+namespace BootCamp.Chapter
 {
     class Program
     {
@@ -9,6 +13,47 @@
             // - FindPersonWithBiggestLoss
             // - FindRichestPerson
             // - FindMostPoorPerson
+            var message = $"Hello{Environment.NewLine}World!";
+            var framed = Framer(message, 0);
+            Console.WriteLine(framed);
         }
+        public static string Framer(string message, int padding)
+
+        {
+            var arr = message.Split($"{Environment.NewLine}");
+            int longerLineSize = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].Length > longerLineSize)
+                {
+                    longerLineSize = arr[i].Length;
+                }
+            }
+
+            var frame = new string[arr.Length + 2];
+
+            frame[0] = "  +";
+            for (var i = 0; i < longerLineSize; i++)
+            {
+                frame[0] += "-";
+            }
+            frame[0] += "+";
+
+            for (var i = 0; i < arr.Length; i++)
+            {
+                frame[i + 1] = $"{Environment.NewLine}  |" + arr[i];
+                for (var j = 0; j < longerLineSize - arr[i].Length; j++)
+                {
+                    frame[i + 1] += " ";
+                }
+                frame[i + 1] += "|";
+            }
+
+            frame[frame.Length-1] = $"{Environment.NewLine}{frame[0]}";
+
+            return String.Join("", frame);
+        }
+
+
     }
 }
