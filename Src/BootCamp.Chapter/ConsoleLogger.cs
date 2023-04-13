2550bb956ea00162bc22c38073aa3a9674951e9c
Pedro Prado Win <126698557+pradoPedroPrado@users.noreply.github.com>
2023-04-12 21:37:37 -0300

    ILogger and ConsoleLogger inplemented. FileLogger left.

---
+++ Src/BootCamp.Chapter/ConsoleLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    internal class ConsoleLogger : ILogger
+    {
+        bool isFirstLog = true;
+        public void Log(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            if (isFirstLog)
+            {
+                Console.Write(DateTime.Now.ToString("MM/dd/yyyy") + " ");
+                isFirstLog = false;
+            }
+            Console.Write(DateTime.Now.ToString("HH:mm:ss"));
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($" - {message}");
+            Console.ResetColor();
+        }
+    }
+}
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,25 +6,37 @@ namespace BootCamp.Chapter
 {
     class Program
     {
+        static readonly ConsoleLogger _logger = new ConsoleLogger();
         static void Main(string[] args)
         {
-            string name = GetPersonData("name");
-            string surename = GetPersonData("surename");
-            int age = int.Parse(GetPersonData("age"));
-            double weight = double.Parse(GetPersonData("weight"));
-            double height = double.Parse(GetPersonData("height in cm"));
-            double BMI = CalculateBMI(weight, height);
-            Console.WriteLine($"{name} {surename} is {age}, weight is {weight} kg and height is {height} cm.");
-            Console.WriteLine($"BMI is {BMI:f2}");
+            try
+            {
+                _logger.Log("Program started.");
+                string name = GetPersonData("name");
+                string surename = GetPersonData("surename");
+                int age = int.Parse(GetPersonData("age"));
+                double weight = double.Parse(GetPersonData("weight"));
+                double height = double.Parse(GetPersonData("height in cm"));
+                double BMI = CalculateBMI(weight, height);
+                Console.WriteLine($"{name} {surename} is {age}, weight is {weight} kg and height is {height} cm.");
+                Console.WriteLine($"BMI is {BMI:f2}");
 
-            name = GetPersonData("name");
-            surename = GetPersonData("surename");
-            age = int.Parse(GetPersonData("age"));
-            weight = double.Parse(GetPersonData("weight"));
-            height = double.Parse(GetPersonData("height in cm"));
-            BMI = CalculateBMI(weight, height);
-            Console.WriteLine($"{name} {surename} is {age}, weight is {weight} kg and height is {height} cm.");
-            Console.WriteLine($"BMI is {BMI:f2}");
+                name = GetPersonData("name");
+                surename = GetPersonData("surename");
+                age = int.Parse(GetPersonData("age"));
+                weight = double.Parse(GetPersonData("weight"));
+                height = double.Parse(GetPersonData("height in cm"));
+                BMI = CalculateBMI(weight, height);
+                Console.WriteLine($"{name} {surename} is {age}, weight is {weight} kg and height is {height} cm.");
+                Console.WriteLine($"BMI is {BMI:f2}");
+                _logger.Log("Program terminated.");
+
+
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(ex.ToString());
+            }        
         }
 
         public static string GetPersonData(string text)
