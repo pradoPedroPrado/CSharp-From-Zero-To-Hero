271790504f0ec85e5850ef6d0fc32ba6c27e1682
Pedro Prado Win <126698557+pradoPedroPrado@users.noreply.github.com>
2023-05-08 09:43:32 -0300

    MostCommonLetterFinder pass all tests

--- Src/BootCamp.Chapter/MostCommonLetterFinder.cs
+++ Src/BootCamp.Chapter/MostCommonLetterFinder.cs
@@ -1,10 +1,38 @@
-﻿namespace BootCamp.Chapter
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BootCamp.Chapter
 {
     public static class MostCommonLetterFinder
     {
         public static char Find(string sentence)
         {
-            return 'z';
+            if (string.IsNullOrEmpty(sentence) || sentence == " ") { throw new ArgumentNullException(nameof(sentence)); }
+            char[] sentenceArr = sentence.ToCharArray();
+            Dictionary<char,int> map = new Dictionary<char,int>();
+            foreach (char ch in sentenceArr)
+            {
+                if (map.ContainsKey(ch))
+                {
+                    map[ch] += 1;
+                }
+                else
+                {
+                    map.Add(ch, 1);
+                }
+            }
+            char mostCommon = map.Keys.FirstOrDefault();
+            int highestValue = 0;
+            foreach (char ch in sentenceArr)
+            {
+                if (map[ch] > highestValue) 
+                { 
+                    highestValue = map[ch];
+                    mostCommon = ch;
+                }
+            }
+            return mostCommon;
         }
     }
 }
